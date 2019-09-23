03b069d9e71baadf8e792a1964703c1f80e7dd7b
flolude <fllu@pm.me>
2019-09-23 22:34:24 +0200

    🃏 add angular app with jest testing

---
+++ services/frontend/jest.config.js
@@ -0,0 +1,28 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+/* module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@assets/(.*)': '<rootDir>/src/assets/$1',
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@env': '<rootDir>/src/environments/environment',
+    '@src/(.*)': '<rootDir>/src/src/$1',
+    '@state/(.*)': '<rootDir>/src/app/state/$1'
+  },
+  transformIgnorePatterns: ['node_modules/(?!(jest-test))']
+};
+ */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      diagnostics: false,
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
+    },
+  },
+};
