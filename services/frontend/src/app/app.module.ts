9a6e32845f866655afdf7ab8dc907997c1809c72
flolude <fllu@pm.me>
2019-09-23 22:48:08 +0200

    🖥️ add server side rendering to angular frontend

--- services/frontend/src/app/app.module.ts
+++ services/frontend/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { environment } from '../environments/environment';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
