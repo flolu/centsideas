ff88366d7347fae5ab4184c52340bc233203d9ad
flolude <fllu@pm.me>
2019-10-19 13:47:57 +0200

    🗃 custom logger

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -1,34 +1,94 @@
 import * as pino from 'pino';
 import { injectable } from 'inversify';
 
-// TODO colors like gltf-converter
+// FIXME refactor all logs to fit new logger
 
 @injectable()
 export class Logger {
-  private logger: pino.Logger;
-  private prefix: string = '[-] ';
+  static levels = {
+    debug: 5,
+    info: 4,
+    success: 3,
+    warn: 2,
+    error: 1,
+  };
+
+  private readonly resetColor = '\x1b[0m';
+  private readonly dimmed = '\x1b[2m';
+  private readonly green = '\x1b[42m';
+  private readonly yellow = '\x1b[33m';
+  private readonly red = '\x1b[31m';
+  private readonly separatorColor = '\x1b[7m';
+  private readonly fatalColor = '\x1b[41m';
+  private prefix = '● ';
+  private level: number;
+  private includeTimestamp = false;
 
   constructor() {
-    const options: pino.LoggerOptions = { prettyPrint: true, level: process.env.LEVEL || 'info' };
+    this.level = this.getLogLevel(process.env.LEVEL || 'debug');
     if (process.env.LOGGER_PREFIX) {
       this.prefix = process.env.LOGGER_PREFIX + ' ';
     }
-    this.logger = pino(options);
   }
 
-  public info = (message: string, ...args: any[]) => {
-    this.logger.info(this.prefix, message, ...args);
+  debug = (...parts: any[]) => {
+    if (this.level >= Logger.levels.debug)
+      console.log(this.dimmed, this.prefix, this.timestamp(), this.fillWithSpaces('debug'), ...parts, this.resetColor);
   };
 
-  public debug = (message: string, ...args: any[]) => {
-    this.logger.debug(this.prefix, message, ...args);
+  info = (...parts: any[]) => {
+    if (this.level >= Logger.levels.info)
+      console.log(
+        this.resetColor,
+        this.prefix,
+        this.timestamp(),
+        this.fillWithSpaces('info'),
+        ...parts,
+        this.resetColor,
+      );
   };
 
-  public warn = (message: string, ...args: any[]) => {
-    this.logger.warn(message, this.prefix, ...args);
+  success = (...parts: any[]) => {
+    if (this.level >= Logger.levels.success)
+      console.log(this.green, this.prefix, this.timestamp(), this.fillWithSpaces('success'), ...parts, this.resetColor);
   };
 
-  public error = (message: string, ...args: any[]) => {
-    this.logger.error(this.prefix, message, ...args);
+  warn = (...parts: any[]) => {
+    if (this.level >= Logger.levels.warn)
+      console.log(this.yellow, this.prefix, this.timestamp(), this.fillWithSpaces('warn'), ...parts, this.resetColor);
+  };
+
+  error = (...parts: any[]) =>
+    console.log(this.red, this.prefix, this.timestamp(), this.fillWithSpaces('error'), ...parts, this.resetColor);
+
+  separator = (...parts: any[]) => console.log(this.resetColor, '\n', this.separatorColor, ...parts, this.resetColor);
+
+  fatal = (...parts: any[]) => console.log(this.fatalColor, ...parts, this.resetColor);
+
+  private getLogLevel = (level: string): number => {
+    switch (level) {
+      case 'error':
+        return 1;
+      case 'warn':
+        return 2;
+      case 'success':
+        return 3;
+      case 'info':
+        return 4;
+      case 'debug':
+        return 5;
+      default:
+        return 5;
+    }
+  };
+
+  private timestamp = () => (this.includeTimestamp ? new Date().toLocaleTimeString() : '');
+
+  private fillWithSpaces = (content: string, endChar: string = ':', totalLength: number = 8) => {
+    let line = content;
+    while (line.length < totalLength) {
+      line += ' ';
+    }
+    return line + endChar;
   };
 }
