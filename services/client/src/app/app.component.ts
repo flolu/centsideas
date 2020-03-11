4ead1c5dad34a063ccea5b6a811cc8b2ee99d0b0
flolu <fllu@pm.me>
2020-03-11 12:36:56 +0100

    remove boilerplate code

--- services/client/src/app/app.component.ts
+++ services/client/src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-component',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>Client Angular App</h1>
+    <h2>app-component</h2>
+  `,
+  styles: [
+    `
+      h2 {
+        color: purple;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'client';
