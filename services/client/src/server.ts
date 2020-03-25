d9ab43415eb2760dcbaf305a9e14f0bdaa6a8b14
flolude <fllu@pm.me>
2020-03-25 13:04:46 +0100

    add universal bazel targets

--- services/client/src/server.ts
+++ services/client/src/server.ts
@@ -1,10 +1,53 @@
+///<reference types="node"/>
 import 'zone.js/dist/zone-node';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
 
-import { AppServerModule } from './src/main.server';
+// Express server
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'services/client/src/prodapp');
+
+import { AppServerModule } from './app/app.server.module';
+
+// Our Universal express-engine (found @
+// https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine(
+  'html',
+  ngExpressEngine({
+    bootstrap: AppServerModule,
+    providers: [
+      // provideModuleMap(LAZY_MODULE_MAP)
+    ],
+  }) as any,
+);
+
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
+// Serve static files from /browser
+app.get('*.*', express.static(DIST_FOLDER, { maxAge: '1y' }));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+  res.render('index', { req });
+});
+
+// Start up the Node server
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
+
+/* import 'zone.js/dist/zone-node';
+
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { join } from 'path';
+
+import { AppServerModule } from './main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
@@ -63,4 +106,5 @@ if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
 
-export * from './src/main.server';
+export * from './main.server';
+ */
