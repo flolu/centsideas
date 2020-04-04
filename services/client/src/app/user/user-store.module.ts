df82828f018ddd98576c6695e069aec5cfde7c8a
flolude <fllu@pm.me>
2020-04-04 12:07:05 +0200

    improve(bazel): split ngrx into feature store module

---
+++ services/client/src/app/user/user-store.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { AuthEffects } from './auth.effects';
+import * as fromAuth from './auth.reducer';
+import * as fromUser from './user.reducer';
+import { UserService } from './user.service';
+
+export const featureKey = 'user';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    // TODO don't hardcode name
+    StoreModule.forFeature(featureKey, { auth: fromAuth.reducer, user: fromUser.reducer }),
+    EffectsModule.forFeature([AuthEffects]),
+  ],
+  providers: [AuthEffects, UserService],
+})
+export class UserStoreModule {}
--- services/client/src/app/user/user.module.ts
+++ services/client/src/app/user/user.module.ts
@@ -2,34 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
 import { HttpClientModule } from '@angular/common/http';
-import { EffectsModule } from '@ngrx/effects';
 
 import { AuthFrontendRoutes } from '@cents-ideas/enums';
 
 import { LoginContainer } from './login.container';
 import { MeContainer } from './me.container';
-import { UserService } from './user.service';
-import { AuthEffects } from './auth.effects';
-import * as fromAuth from './auth.reducer';
-import * as fromUser from './user.reducer';
 import { AuthGuard } from './auth.guard';
+import { UserStoreModule } from './user-store.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    UserStoreModule,
     RouterModule.forChild([
       { path: AuthFrontendRoutes.Login, component: LoginContainer },
       { path: AuthFrontendRoutes.Me, component: MeContainer, canActivate: [AuthGuard] },
     ]),
-    // TODO don't hardcode name
-    StoreModule.forFeature('users', { auth: fromAuth.reducer, user: fromUser.reducer }),
-    EffectsModule.forFeature([AuthEffects]),
-    HttpClientModule,
   ],
-  providers: [UserService, AuthEffects, AuthGuard],
+  providers: [AuthGuard],
   declarations: [LoginContainer, MeContainer],
 })
 export class UserModule {}
