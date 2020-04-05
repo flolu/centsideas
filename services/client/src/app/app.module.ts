71e15f97a7fe88d0d9f0c5d5e1c08d35bfb5c9de
flolude <fllu@pm.me>
2020-04-05 12:01:31 +0200

    feat(client): add ngrx router connecting module

--- services/client/src/app/app.module.ts
+++ services/client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,14 +13,16 @@ import { UserModule } from './user/user.module';
 import { AuthTokenInterceptor } from './auth-token.interceptor';
 import { NgRxStateTransferService, setTransferedState } from './ngrx-state-transfer.service';
 import { env } from '../environments';
+import { CustomSerializer } from './custom-route-serializer';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'client' }),
     BrowserTransferStateModule,
-    StoreModule.forRoot({}, { metaReducers: [setTransferedState] }),
+    StoreModule.forRoot({ router: routerReducer }, { metaReducers: [setTransferedState] }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({ serializer: CustomSerializer }),
     // TODO only in dev mode (maybe inject at runtime in dev mode?)
     StoreDevtoolsModule.instrument({ logOnly: env.production }),
     AppRoutingModule,
---
+++ services/client/src/app/custom-route-serializer.ts
@@ -0,0 +1,26 @@
+import { Params, RouterStateSnapshot } from '@angular/router';
+import { RouterStateSerializer } from '@ngrx/router-store';
+
+export interface RouterStateUrl {
+  url: string;
+  params: Params;
+  queryParams: Params;
+}
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    let route = routerState.root;
+
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const {
+      url,
+      root: { queryParams },
+    } = routerState;
+    const { params } = route;
+
+    return { url, params, queryParams };
+  }
+}
