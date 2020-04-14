74fa59c77c866232e08d05f8d81fbb52292fdbe0
flolu <fllu@pm.me>
2020-04-14 11:09:48 +0200

    fix(client): gateway defaulted to wrong url in cached javascript

--- services/client/shared/environment/environment.service.ts
+++ services/client/shared/environment/environment.service.ts
@@ -1,28 +1,45 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { makeStateKey, TransferState } from '@angular/platform-browser';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 
 import { IEnvironment } from './environment.model';
 
 @Injectable()
 export class EnvironmentService {
-  private keyName = '@cents-ideas/environment';
-  private key = makeStateKey(this.keyName);
-
-  // TODO there shouldn't be a default, but rather the gatewayHost should always be transferred from the server?!
-  // TODO plus error if connection to api can't be established
-  private readonly defaultEnvironment = { gatewayHost: 'http://localhost:3000', isDefault: true };
-  private environment: IEnvironment = this.defaultEnvironment;
+  // FIXME dont hardcode
+  private readonly defaultGatewayHost = 'https://api.cents-ideas.flolu.com';
+  private readonly localstorageEnvironmentKey = 'environment';
+  private readonly keyName = '@cents-ideas/environment';
+  private readonly defaultEnvironment: IEnvironment = { gatewayHost: this.defaultGatewayHost };
+  private readonly key = makeStateKey(this.keyName);
+  private environment: IEnvironment;
 
   constructor(
     private transferState: TransferState,
     @Inject(PLATFORM_ID) private platform: string,
-  ) {}
+    @Inject(DOCUMENT) private document: Document,
+  ) {
+    if (isPlatformBrowser(this.platform)) {
+      const environment: IEnvironment | null = JSON.parse(
+        localStorage.getItem(this.localstorageEnvironmentKey),
+      );
+      if (environment) this.environment = environment;
+      else {
+        if (this.document.location.hostname === 'localhost')
+          // FIXME dont hardcode
+          this.environment = { gatewayHost: 'http://localhost:3000' };
+        else this.environment = this.defaultEnvironment;
+      }
+    } else {
+      this.environment = this.defaultEnvironment;
+    }
+  }
 
   handleStateTransfer = () => {
     if (isPlatformBrowser(this.platform)) {
       if (this.transferState.hasKey(this.key)) {
         const environment: IEnvironment = this.transferState.get(this.key, null);
+        localStorage.setItem(this.localstorageEnvironmentKey, JSON.stringify(environment));
         this.environment = environment;
       }
     } else {
