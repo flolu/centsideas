117749fd8680c616c3bfeb93af86a77301602a01
flolu <fllu@pm.me>
2020-04-24 21:55:06 +0200

    fix: untrack auto-generated files

--- services/admin-client/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable no-invalid-template-strings */
-
-import { IAdminClientEnvironment } from '.';
-
-export const environment: IAdminClientEnvironment = {
-  gatewayUrl: 'http://localhost:3000',
-};
--- services/client/environment/environment.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable no-invalid-template-strings */
-
-import { IClientEnvironment } from '.';
-
-export const environment: IClientEnvironment = {
-  gatewayUrl: 'http://localhost:3000',
-  vapidPublicKey:
-    'BAbf0XZl9N1XWzNUrhfLFvzsYh-g_eKx0xtQdXoFn2Z99f-d68dcpRu4EheMPY7oO_nRFzmkwkLNbX2DSTpr3fA',
-};
